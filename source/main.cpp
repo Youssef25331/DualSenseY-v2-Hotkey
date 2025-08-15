e9f45c8bbc235b090f362662178843c356c76b88
Youssef25331 <yousseft25331@gmail.com>
2025-08-15 19:34:55 +0300

    args test

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[]) {
 }
 #else
 #include <Windows.h>
+#include <shellapi.h>
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                      LPTSTR lpCmdLine, int nCmdShow) {
@@ -14,7 +15,21 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
   // Shows a message if program is running
   CreateMutexA(0, FALSE, "Local\\$DualSenseY$");
   if (GetLastError() == ERROR_ALREADY_EXISTS) {
-    MessageBoxW(NULL, L"Program already running", L"Failed to start", MB_OK);
+    int argc;
+    int commmand_index;
+    bool is_command = false;
+    LPWSTR *argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+    for (int i = 0; i < argc; i++) {
+      if (!wcscmp(argv[i], L"--command")) {
+        is_command = true;
+        commmand_index = i + 1;
+      }
+    }
+    if (is_command) {
+      MessageBoxW(NULL, argv[commmand_index], L"Found Command", MB_OK);
+    } else {
+      MessageBoxW(NULL, L"Program already started", L"Error", MB_OK);
+    }
     return -1;
   }
 
